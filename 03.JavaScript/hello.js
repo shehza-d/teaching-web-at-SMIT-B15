8e5bb38fe248f1e71eb7b2b27c5916a7670dbd66
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-02-17 11:56:21 +0500

    💯 Intro to programming js class 1

---
+++ 03.JavaScript/hello.js
@@ -0,0 +1,12 @@
+let myName = "usman"
+
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(2+2)
