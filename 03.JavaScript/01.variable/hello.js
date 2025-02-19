e4021f572accf09e4bbecc57b69ec38fdd941407
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-02-19 11:15:47 +0500

    CLASS: JS error and assigning

---
+++ 03.JavaScript/01.variable/hello.js
@@ -0,0 +1,17 @@
+let myName = "usman";
+
+// all letters are allowed
+// variable name ma saruf 2 symbol allow hyn $ aur _ 
+// variable name ma number aasagty hyn middle and end ma not in start
+
+console.log(myName)
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+
+console.log(2 + 2);
---
+++ 03.JavaScript/02.prompt/app.js
@@ -0,0 +1,13 @@
+let testScore = "shehzad"; // declaration
+
+console.log(testScore);
+
+// console.log(test); // error ky baad code nhi chalta aur break ho jata hy
+
+// code line by line chalta hy
+
+testScore = 67; // assigning
+
+console.log(testScore);
+
+console.log(testScore);
