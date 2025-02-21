09a8229d4b5b334862c35bda7d670dfd79cde4d3
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-02-21 11:21:19 +0500

    CLASS: const, prompt, cases

--- 03.JavaScript/02.prompt/app.js
+++ 03.JavaScript/02.prompt/app.js
@@ -11,3 +11,44 @@ testScore = 67; // assigning
 console.log(testScore);
 
 console.log(testScore);
+
+let name = "shehzad"; // string (text hi string hota hy)
+
+let test = -6;
+let smit = 7.6; // number
+
+console.log(test + smit);
+
+let switchButton = true;
+
+// prompt user se value lene ky liye
+let userName = prompt("apna name batao? 2");
+console.log(userName);
+
+// const change nhi hota
+const email = "shehzad@mail.com";
+
+email = "pakistan"; // TypeError
+
+console.log("ye mera email hy " + email);
+
+// their are 3 type of variable
+// var
+// let
+// const
+// using
+
+// $ and _
+let myAge = 30;
+
+let hello_my_name_is_shehzad; // snake_case
+let helloMyNameIsShehzad2; // camelCase
+// let hello-my-name-is-shehzad; // kabab-case
+
+let price = 45;
+
+price = price - 10; // 10 rupe ka discount dena hy is line per
+
+console.log("value:" + price);
+
+// page 18
