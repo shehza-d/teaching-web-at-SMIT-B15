46b7d4f70c22dbff3511a841c51588a41fab0606
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-02-24 12:11:36 +0500

    CLASS: data types

---
+++ 03.JavaScript/03.alert-practice/main.js
@@ -0,0 +1,32 @@
+let name = "";
+let email = "";
+let password = "";
+let contact = "";
+let gender = "";
+
+alert("welcome to user");
+
+alert("Enter your name ");
+name = prompt("please enter your name ");
+
+alert("enter your email");
+email = prompt("enter your email");
+
+alert("enter your password");
+password = prompt("enter your password");
+
+alert("enter your contact");
+contact = prompt("enter your contact");
+
+alert("enter your gender");
+gender = prompt("enter your gender");
+
+console.table([name, email, password, contact, gender]);
+
+console.log(name, email, password, contact, gender);
+
+// js is case sensitive
+let username = "shehzad";
+let UserName = "ahmed";
+
+console.log(username);
