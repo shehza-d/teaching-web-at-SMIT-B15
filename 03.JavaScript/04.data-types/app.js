04edc12689318e6e18ad9a4c50ef46b6155d8bf2
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-03-03 10:04:19 +0500

    CLASS: Comparison operators

---
+++ 03.JavaScript/04.data-types/app.js
@@ -0,0 +1,58 @@
+let myName = `Shehzad`; // String
+let age = 50; // Number
+
+// Boolean
+let lightSwitch = false;
+let lightSwitch2 = true;
+
+// undefined
+let test = undefined;
+let test1;
+
+// null
+let empty = null;
+
+// symbol
+// bigint
+
+// Primitive data types are 7
+// 1. String "" , '' ``
+// 2. Number
+// 3. Boolean = true , false
+// 4. undefined
+// 5. null
+// 6. BigInt
+// 7. Symbol
+
+// symbol is always different
+// console.log(Symbol("a") == Symbol("a"));
+
+// Comparison operators
+// Comparison operators always returns boolean
+// https://www.w3schools.com/js/js_comparisons.asp
+
+// more strict === (data type bhi check krta hy )
+// console.log(2 === true);
+
+// console.log(4 === 4);
+// console.log(typeof 4 == 5);
+
+// console.log("4" == 4);
+// console.log("4" === 4);
+// console.log(typeof (4 == 4));
+
+let myName2 = "shEhzad";
+
+// console.log(myName2 === "shehzad");
+
+// console.log("owais" !== "owais");
+// console.log(2 !== 3);
+// console.log(0 !== 0);
+
+// console.log("" !== "dd"); // jab bhi same values nhi hongyi tw true return hoga
+// console.log(true !== true);
+
+// console.log(5 < 5.00001);
+// console.log(6.9 < 6.0001);
+
+// console.log(5 >= 5.00001);
