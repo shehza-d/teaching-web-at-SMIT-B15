ead4cdb79769d58c608fe496fdddec0edc2e0d3c
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-03-05 10:14:46 +0500

    CLASS: if else

---
+++ 03.JavaScript/06.if-else/app.js
@@ -0,0 +1,31 @@
+// converting string to number
+let a = "32";
+
+// a = Number(a);
+// a = parseInt(a);
+// a = parseFloat(a);
+// a = +a;
+
+// console.log(a);
+// console.log(typeof a);
+
+// converting number to string
+let b = 56;
+
+// b = b.toString()
+b = String(b);
+
+// console.log(b);
+// console.log(typeof b);
+
+let temperature = 13;
+
+if (temperature < 20) {
+  console.log("Jacket lelo!!!");
+} else {
+  console.log("jacket bach do!!!");
+}
+
+// if (Boolean) {
+// boolean true hy tw ye code chale ga
+// }
