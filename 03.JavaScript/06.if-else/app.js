94c87d169462dad2bb42035fffcad46e60d30dfb
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-03-14 09:50:10 +0500

    CLASS: input getting values

--- 03.JavaScript/06.if-else/app.js
+++ 03.JavaScript/06.if-else/app.js
@@ -18,14 +18,53 @@ b = String(b);
 // console.log(b);
 // console.log(typeof b);
 
-let temperature = 13;
-
-if (temperature < 20) {
-  console.log("Jacket lelo!!!");
-} else {
-  console.log("jacket bach do!!!");
-}
+// agar
+// if (boolean) {
+// }
 
 // if (Boolean) {
 // boolean true hy tw ye code chale ga
 // }
+
+let temperature = 20;
+
+// 20 pr sardi
+
+// if (temperature <= 20) {
+//   // ye code chale ga
+//   console.log("Sardi hy 🥶");
+// } else {
+//   console.log("Garmi hy 🌞");
+// }
+
+// numberInput1.style.color = 'red'
+// ----------------
+let myForm = document.querySelector("#mera-form");
+let numberInput1 = document.querySelector("#num1");
+let numberInput2 = document.querySelector("#num2");
+let resultPara = document.querySelector("#result");
+
+
+
+
+// console.log(myForm);
+// console.log(numberInput1);
+// console.log(numberInput2);
+
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // andar ka code saruf tab chale ga jab form submit hoga
+
+  console.log("form submit hua hy");
+
+  let num1 = Number(numberInput1.value);
+  let num2 = Number(numberInput2.value);
+
+  const sum = num1 + num2
+ 
+  console.log(sum);
+
+  resultPara.innerText = sum
+
+  // event.target.reset()
+});
