a13e9720242dc562ebd6412b97366814349d172a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-03-19 10:15:15 +0500

    CLASS: array

---
+++ 03.JavaScript/10.Array/app.js
@@ -0,0 +1,23 @@
+let age = 50;
+let res = "pak";
+
+// (false || true && false)
+// (false || false)// false
+
+let a = 5;
+let b = a++;
+
+console.log(a); // 6
+console.log(b); // 5
+
+if (card) {
+  if (attendace) {
+    console.log("aajao");
+  }
+}
+
+// array aur object hamesha const se banta hy
+const fruits = ["apple", "banana", "mango", "grapes"];
+// getting last element
+
+console.log(fruits[fruits.length - 1]);
