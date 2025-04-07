2b4777a830b05742e30099ef6785f44ef8721cff
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-04-07 10:47:35 +0500

    CLASS: Loops

---
+++ 03.JavaScript/13.loops/script.js
@@ -0,0 +1,25 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.log("shehzad");
+
+for (1; 2; 4) {
+  3;
+}
+
+let a = 10;
+// both do same work
+a = a + 2;
+a += 2;
+
+for (initialization; condition; increment) {
+  loop_body;
+}
+
+let i = 0;
+
+for (console.log("shehzad"); i < 10; console.log("increment")) {
+  console.log(i);
+
+  ++i;
+}
