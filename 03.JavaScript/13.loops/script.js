149cd240050602f15804e80d2b63c8e284597509
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2025-04-09 11:11:18 +0500

    CLASS: array with loops

--- 03.JavaScript/13.loops/script.js
+++ 03.JavaScript/13.loops/script.js
@@ -1,25 +1,67 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// console.log("shehzad");
+
+// for (1; 2; 4) {
+//   3;
+// }
+
+// let a = 10;
+// // both do same work
+// a = a + 2;
+// a += 2;
+
+// for (initialization; condition; increment) {
+//   loop_body;
+// }
+
+// let i = 0;
+
+// for (console.log("shehzad"); i < 10; console.log("increment")) {
+//   console.log(i);
+
+//   ++i;
+// }
+
+// for (let i = 7; i >= 3 ; i--){
+//   console.log(i);
+// }
+// 7
+// 6
+// 5
+// 4
+// 3
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5 || i === 3) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+const arr1 = ["shehzad", "fatima", "ayesha"];
+
+// console.log(arr1[0]);
+// console.log(arr1[1]);
+// console.log(arr1[2]);
+// console.log(arr1[4]);
+// console.log(arr1[5]);
+
+// Printing all elements of Array with loop
+for (let i = 0; i < arr1.length; i++) {
+  // console.log(arr1[i]);
 }
-console.log("shehzad");
 
-for (1; 2; 4) {
-  3;
+const arr2 = [3, 4, 5, 8, 1, 0, 16];
+
+// console.log(arr2[1 + 2] + arr2[1]);
+
+let sum = 0; // 3,7,12,20
+
+for (let i = 0; i < arr2.length; i++) {
+  sum = sum + arr2[i];
 }
 
-let a = 10;
-// both do same work
-a = a + 2;
-a += 2;
-
-for (initialization; condition; increment) {
-  loop_body;
-}
-
-let i = 0;
-
-for (console.log("shehzad"); i < 10; console.log("increment")) {
-  console.log(i);
-
-  ++i;
-}
+console.log("sum", sum);
